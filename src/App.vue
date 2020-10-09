c696cb935bd26253f30729b2c7f44c5575c5ed95
deannabosschert <deanna.bosschert@gmail.com>
2020-10-09 23:47:14 +0200

    fix: weird positioning of main

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <header>
-      <router-link class="homeSymbol" to="/">&#127968; home</router-link>
-      <router-link class="infoSymbol" to="/info">&#8505; info</router-link>
+      <!-- <router-link class="homeSymbol" to="/">&#127968; home</router-link> -->
+      <!-- <router-link class="infoSymbol" to="/info">&#8505; info</router-link> -->
     </header>
     <router-view></router-view>
   </div>
@@ -17,17 +17,17 @@ export default {
 </script>
 
 <style lang="scss">
-.homeSymbol,
-.infoSymbol {
-  font-size: 2rem;
-  font-variant: small-caps;
-  font-weight: 600;
-  color: #a31a1a;
-  display: flex;
-}
-
-header {
-  position: absolute;
-  margin-left: 1rem;
-}
+// .homeSymbol,
+// .infoSymbol {
+//   font-size: 2rem;
+//   font-variant: small-caps;
+//   font-weight: 600;
+//   color: #a31a1a;
+//   display: flex;
+// }
+//
+// header {
+//   position: absolute;
+//   margin-left: 1rem;
+// }
 </style>
