95d87ac7e77f3a464b7b479eea52f730246e2e24
deannabosschert <deanna.bosschert@gmail.com>
2020-10-10 20:24:11 +0200

    feat: add bar chart

--- src/components/nike.vue
+++ src/components/nike.vue
@@ -1,26 +1,33 @@
 <template>
-  <div :id="category">
-    <h2 class="nikeHeader">{{ title }}</h2>
-    <div class="content">
-      <a v-for="(result, index) in results" :key="index" :href="result.url">
-        <section>
-          <h3>{{ result.title.value }}</h3>
-          <p>&#x25bc; {{ result.location.value }}</p>
-          <img
-            class="images"
-            v-bind:src="result.img.value"
-            alt="result.title.value"
-          />
-        </section>
-      </a>
-    </div>
+<section :id="category">
+  <h2 class="nikeHeader">{{ title }}</h2>
+  <div>
+    <bar-chart
+      title="dingenEnzo"
+    />
   </div>
+  <article id="stats-1" active>
+    <h2 class="nikeHeader">Nike</h2>
+    <p class="nikeIntro">Nice. Dan heb je dus dezelfde voorkeur als..</p>
+    <div class="container">
+      <canvas id="myPieChart4" width="300px" height="300px"></canvas>
+    </div>
+    <h3>13% van de studenten in Information Design!</h3>
+    <p>Zou je nog meer gemeen hebben met deze studenten dan alleen jullie lievelingsmerk?</p>
+    <button class="nikeButton1" href="#nikeStats2">Let's find out!</button>
+  </article>
+</section>
 </template>
 
 <script>
 /*eslint 'no-console':0*/
+import BarChart from './charts/bar-chart.js'
+
 export default {
   name: "category",
+  components: {
+    BarChart,
+  },
   data() {
     return {
       results: []
