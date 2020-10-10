9d2b7df6c45904987e43c9a11cfd11e64a9f0695
deannabosschert <deanna.bosschert@gmail.com>
2020-10-10 23:34:52 +0200

    feat: add event listener on button

--- src/components/switchBetween.vue
+++ src/components/switchBetween.vue
@@ -1,19 +1,38 @@
 <template>
-<form class="switchBetween" @change="onSwitch">
-  <input type="radio" id="nike" class="switchBetween_radio" name="switchBetween-input" value="nike" checked />
-  <label for="nike" class="switchBetween_label">
-    Nike
-  </label>
-  <input type="radio" id="adidas" class="switchBetween_radio" name="switchBetween-input" value="adidas" />
-  <label for="adidas" class="switchBetween_label">
-    Adidas
-  </label>
+  <form class="switchBetween" @change="onSwitch">
+    <input
+      type="radio"
+      id="nike"
+      class="switchBetween_radio"
+      name="switchBetween-input"
+      value="nike"
+      checked
+    />
+    <label for="nike" class="switchBetween_label">
+     Nike
+    </label>
+    <input
+      type="radio"
+      id="adidas"
+      class="switchBetween_radio"
+      name="switchBetween-input"
+      value="adidas"
+    />
+    <label for="adidas" class="switchBetween_label">
+      Adidas
+    </label>
 
-  <input type="radio" id="idgaf" class="switchBetween_radio" name="switchBetween-input" value="idgaf" />
-  <label for="idgaf" class="switchBetween_label">
-    Idgaf
-  </label>
-</form>
+    <input
+      type="radio"
+      id="idgaf"
+      class="switchBetween_radio"
+      name="switchBetween-input"
+      value="idgaf"
+    />
+    <label for="idgaf" class="switchBetween_label">
+      Idgaf
+    </label>
+  </form>
 </template>
 
 <script>
@@ -22,9 +41,11 @@ export default {
     onSwitch(event) {
       if (event.target.value === "nike") {
         return this.$emit("nike-click")
-      } else if (event.target.value === "adidas") {
+      }
+      else if (event.target.value === "adidas") {
         return this.$emit("adidas-click")
-      } else if (event.target.value === "idgaf") {
+      }
+      else if (event.target.value === "idgaf") {
         return this.$emit("idgaf-click")
       }
     }
@@ -34,27 +55,27 @@ export default {
 
 <style lang="scss">
 .switchBetween {
-    display: flex;
-    padding: 2px;
-    border-radius: 10px;
-    justify-content: center;
+  display: flex;
+  padding: 2px;
+  border-radius: 10px;
+  justify-content: center;
 }
 
 .switchBetween_label {
-    padding: 1.2rem;
-    border-radius: 5px;
-    background: white;
-    text-align: center;
-    align-self: center;
-    font-weight: 600;
+  padding: 1.2rem;
+  border-radius: 5px;
+  background: white;
+  text-align: center;
+  align-self: center;
+  font-weight: 600;
 }
 
 .switchBetween_radio:checked + .switchBetween_label {
-    background: black;
-    color: white;
+  background: black;
+  color: white;
 }
 
 input[type="radio"] {
-    visibility: hidden;
+  visibility: hidden;
 }
 </style>
