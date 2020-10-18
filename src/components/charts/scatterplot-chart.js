4d193d3beb9a85c83be8e201f52b55b37007c588
deannabosschert <deanna.bosschert@gmail.com>
2020-10-18 14:01:08 +0200

    feat: add custom ticks && labels

--- src/components/charts/scatterplot-chart.js
+++ src/components/charts/scatterplot-chart.js
@@ -66,43 +66,44 @@ export default {
           borderColor: 'rgba(232,208,30,1)',
           backgroundColor: 'rgba(232,208,30,0.6)',
           data: [{
-              x: -8,
-              y: -1
-            }, {
-              x: 2,
-              y: 3
-            }, {
-              x: 0,
-              y: 8
-            }, {
-              x: 5,
-              y: -2
-            }, {
-              x: 7,
-              y: -3
-            }]},
-            {
-              label: 'Anders',
-              fill: false,
-              borderColor: 'rgba(119,136,153,1)',
-              backgroundColor: 'rgba(119,136,153,0.6)',
-              data: [{
-                x: -4,
-                y: -2
-              }, {
-                x: -6,
-                y: -6
-              }, {
-                x: 3,
-                y: 7
-              }, {
-                x: 5,
-                y: -3
-              }, {
-                x: 1,
-                y: -2
-              }]
-            }
+            x: -8,
+            y: -1
+          }, {
+            x: 2,
+            y: 3
+          }, {
+            x: 0,
+            y: 8
+          }, {
+            x: 5,
+            y: -2
+          }, {
+            x: 7,
+            y: -3
+          }]
+        },
+        {
+          label: 'Anders',
+          fill: false,
+          borderColor: 'rgba(119,136,153,1)',
+          backgroundColor: 'rgba(119,136,153,0.6)',
+          data: [{
+            x: -4,
+            y: -2
+          }, {
+            x: -6,
+            y: -6
+          }, {
+            x: 3,
+            y: 7
+          }, {
+            x: 5,
+            y: -3
+          }, {
+            x: 1,
+            y: -2
+          }]
+        }
       ]
     }, {
       responsive: true,
@@ -110,6 +111,39 @@ export default {
       title: {
         display: true,
         text: this.title
+      },
+      scales: {
+        yAxes: [{
+          id: 'left-y-axis',
+          type: 'linear',
+          position: 'left',
+          scaleLabel: {
+            display: true,
+            labelString: 'Grootte slaapkamer',
+            fontSize: 15,
+          },
+          ticks: {
+            suggestedMin: -5,
+            suggestedMax: 10,
+            stepSize: 2,
+            // Include m²-sign in the ticks
+            callback: function(value) {
+              return value + 'm²'
+            }
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Hoeveelheid kledingstukken in bezit',
+            fontSize: 15,
+          },
+          ticks: {
+            suggestedMin: -5,
+            suggestedMax: 10,
+            stepSize: 2
+          }
+        }]
       }
     })
   }
