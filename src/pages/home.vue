5d529da9bf9c7af254fd33fc53c7e5f0cd4ca6cb
techkech <deanna.bosschert@upcmail.nl>
2020-10-20 23:59:59 +0200

    feat: switch in place

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,15 +5,15 @@
     <SwitchBetween class="choices" @nike-click="choice = 'nike'" @adidas-click="choice = 'adidas'" @geen-click="choice = 'geen'" />
 
     <div v-if="choice === 'nike'">
-      <BrandChoice title="Nike" category="nike" piechartpercentage="13" introbrand="Nike als" brand="Nike" voorkeur="een visje" branditem="de geliefde Nike-joggers"/>
+      <BrandChoice title="Nike" category="nike" piechartpercentage="13" introbrand="Nike als" brand="Nike" voorkeur="een visje" branditem="de geliefde Nike-joggers" happinessquote="best wel prima zit zo dan! Only happy thoughts <3"/>
     </div>
 
     <div v-if="choice === 'adidas'">
-      <BrandChoice title="Adidas" category="adidas" piechartpercentage="6" introbrand="Adidas als" brand="Adidas" voorkeur="een techkech" branditem="de welbekende Adidas-jacks"/>
+      <BrandChoice title="Adidas" category="adidas" piechartpercentage="6" introbrand="Adidas als" brand="Adidas" voorkeur="een techkech" branditem="de welbekende Adidas-jacks" happinessquote="misschien voortaan beter voor een ander merk kunt gaan.. anything other than Adidas in ieder geval 🧐"/>
     </div>
 
     <div v-if="choice === 'geen'">
-      <BrandChoice title="Geen merk" category="geen" piechartpercentage="6" introbrand="het ontbreken van een" brand="Geen" voorkeur="niet te plaatsen" branditem="uhh.. unbranded jacks/joggers?"/>
+      <BrandChoice title="Geen merk" category="geen" piechartpercentage="6" introbrand="het ontbreken van een" brand="Geen" voorkeur="niet te plaatsen" branditem="uhh.. unbranded jacks/joggers?" happinessquote="best wel prima zit zo dan! Misschien één Nike-petje in huis halen om nét wat extra zonnestralen naar binnen te halen? 🌞"/>
     </div>
   </div>
 </main>
@@ -27,7 +27,7 @@ export default {
   name: "home",
   data() {
     return {
-      choice: "nike"
+      choice: ""
     };
   },
   methods: {},
