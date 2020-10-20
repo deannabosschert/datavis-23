0e9b96c15aaa58e33beb2a4895ba1de714f6d980
techkech <deanna.bosschert@upcmail.nl>
2020-10-20 21:11:02 +0200

    feat: move changes from other repo

--- src/components/charts/bar-chart.js
+++ src/components/charts/bar-chart.js
@@ -1,7 +1,9 @@
-import { Bar } from 'vue-chartjs'
+import {
+  HorizontalBar
+} from 'vue-chartjs'
 
 export default {
-  extends: Bar,
+  extends: HorizontalBar,
   props: {
     data: ['data', 'options'],
     title: {
@@ -10,45 +12,71 @@ export default {
     }
   },
   components: {
-    Bar
+    HorizontalBar
   },
   mounted() {
     this.renderBarChart()
   },
   methods: {
+    /*eslint 'no-console':0*/
     renderBarChart() {
-      this.renderChart(
-        {
-          labels: ['Nike', 'Adidas', 'Geen', 'Anders'],
-          datasets: [
-            {
-              data: [11, 5, 5, 62],
-              label: this.title,
-              backgroundColor: [
-                'rgba(226,20,45, 0.5)',
-                'rgba(55,23,119, 0.5)',
-                'rgba(211,211,211, 0.5)',
-                'rgba(119,136,153, 0.4)'
-              ],
-              borderColor: [
-                'rgba(226,20,45, 1)',
-                'rgba(55,23,119, 1)',
-                'rgba(211,211,211, 1)',
-                'rgba(119,136,153, 1)'
-              ],
-              borderWidth: 1
-            }
-          ]
+      this.renderChart({
+        labels: ['Nike', 'Adidas', 'Geen', 'Anders'],
+        datasets: [{
+          data: ['7.636363636', '5.8', '7.274193548', '7.6'],
+          // label: this.label,
+          backgroundColor: [
+            'rgba(226,20,45,0.55)',
+            'rgba(0,81,186,0.55)',
+            'rgba(232,208,30,0.55)',
+            'rgba(119,136,153,0.55)'
+          ],
+          borderColor: [
+            'rgba(226,20,45,1)',
+            'rgba(0,81,186,1)',
+            'rgba(232,208,30,1)',
+            'rgba(119,136,153,1)'
+          ],
+          borderWidth: 1
+        }]
+      }, {
+        responsive: true,
+        title: {
+          display: true,
+          text: this.title
         },
-        {
-          responsive: true,
-          title: {
-            display: true,
-            text: this.title
-          },
-          maintainAspectRatio: true
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "Lievelingsmerk",
+              fontSize: 15,
+            },
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 10,
+              stepSize: 1,
+            }
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Hoe tevreden ben je met je leven?',
+              fontSize: 15,
+            },
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 10,
+              stepSize: 1
+            }
+          }]
+        },
+        legend: {
+          display: false
+
         }
-      )
+      })
     }
   }
 }
--- src/components/nike.vue
+++ src/components/nike.vue
@@ -1,7 +1,7 @@
 <template>
 <section :id="category">
   <h2 class="nikeHeader">{{ title }}</h2>
-  <div>
+  <div class="chart-ding">
     <pie-chart title="Favoriete merk onder ID20-studenten" />
   </div>
   <article id="stats-1">
@@ -15,7 +15,14 @@
   <transition name="fade">
     <article id="stats-2" v-if="showStats2">
       <h3>meer stats!</h3>
-      <div>
+      <div class="chart-ding">
+        <radar-chart-2019 title="CMD-voorkeuren 2019" />
+        <radar-chart-2020 title="CMD-voorkeuren 2020" />
+      </div>
+      <div class="chart-ding">
+        <radar-chart-amount title="Favoriete merken per jaar" />
+      </div>
+      <div class="chart-ding">
         <line-chart title="Favoriete merk onder ID20-studenten" />
       </div>
       <a class="nikeButton1" v-on:click="[showStats3 = !showStats3, reverseButton2()]" href="#" v-scroll-to="'#stats-3'">{{ button2 }}</a>
@@ -24,7 +31,7 @@
   <transition name="fade">
     <article v-if="showStats3">
       <h3 id="stats-3">Verhouding kleding/space</h3>
-      <div>
+      <div class="scatter-chart">
         <scatter-chart title="Verhouding kleding/kastruimte ID20-studenten" />
       </div>
       <a class="nikeButton1" v-on:click="[showStats4 = !showStats4, reverseButton3()]" href="#" v-scroll-to="'#stats-4'">{{ button3 }}</a>
@@ -32,9 +39,10 @@
   </transition>
   <transition name="fade">
     <article id="stats-4" v-if="showStats4">
-      <h3>nog veel meer stats!</h3>
-      <div>
-        <bar-chart title="Favoriete merk onder ID20-studenten" />
+      <h3>Maakt een merk gelukkig?</h3>
+      <p>Hierbij is gekeken naar het cijfer dat de student met dit merk antwoordde op de vraag "Hoe tevreden ben je met je leven?". Het betreft het gemiddelde antwoord, berekend over de responses binnen het desbetreffende merk.</p>
+      <div class="bar-chart">
+        <bar-chart title="Happiness-rating per favoriet merk" />
       </div>
     </article>
   </transition>
@@ -46,6 +54,9 @@
 import BarChart from './charts/bar-chart.js'
 import PieChart from './charts/pie-chart.js'
 import LineChart from './charts/line-chart.js'
+import RadarChart2019 from './charts/radar-chart-2019.js'
+import RadarChart2020 from './charts/radar-chart-2020.js'
+import RadarChartAmount from './charts/radar-chart-amount.js'
 import ScatterChart from './charts/scatterplot-chart.js'
 
 export default {
@@ -54,6 +65,9 @@ export default {
     BarChart,
     PieChart,
     LineChart,
+    RadarChart2019,
+    RadarChart2020,
+    RadarChartAmount,
     ScatterChart
   },
   data() {
@@ -62,7 +76,7 @@ export default {
       showStats3: true,
       showStats4: true,
       button1: "Let's find out!",
-      button2: "meer dingen",
+      button2: "Ik wil meer weten",
       button3: "nog meer dingen"
     };
   },
@@ -77,10 +91,10 @@ export default {
       }
     },
     reverseButton2() {
-      if (this.button2 === "meer dingen") {
+      if (this.button2 === "Ik wil meer weten") {
         this.button2 = this.button2.replace(/meer/i, 'minder');
         return
-      } else if (this.button2 === "minder dingen") {
+      } else if (this.button2 === "Ik wil minder weten") {
         this.button2 = this.button2.replace(/minder/i, 'meer');
         return
       }
