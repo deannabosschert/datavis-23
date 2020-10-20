7e35ee2b4e9b34e3b0f003e8ee27ac666d6859c7
techkech <deanna.bosschert@upcmail.nl>
2020-10-21 00:47:14 +0200

    feat: change responsiveness to 'false'

--- src/components/charts/pie-chart.js
+++ src/components/charts/pie-chart.js
@@ -43,7 +43,7 @@ export default {
           borderWidth: 1
         }]
       }, {
-        responsive: true,
+        responsive: false,
         title: {
           display: true,
           text: this.title
